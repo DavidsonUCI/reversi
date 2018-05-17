3afe561b51d4ed7fe186ba936df1712e30d0c943
Davidson Young <hello@davidsonyoung.com>
2018-05-17 15:41:08 -0700

    Updated js

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ function getURLParameters(whichParam)
   var pageURL = window.location.search.substring(1);
   var pageURLVariables = pageURL.split('&');
   for(var i = 0; i < pageURLVariables.length; i++) {
-    var parameterName = pageURLVariables[i].split('=');
+    var parameterName = pageURLVariables[i].split('-');
     if(parameterName[0] == whichParam){
       return parameterName[1];
     }
@@ -20,3 +20,12 @@ if('undefined' == typeof username || !username){
 
 
 $('#messages').append('<h4>'+username+'</h4>');
+
+
+/* Connect to the socket server*/
+
+var socket = io.connect();
+
+socket.on('log',function(array){
+  console.log.apply(console,array);
+});
